65822c7c648d97bc261f5084a5281e201b59b325
Emma Guy <emmag87@gmail.com>
2016-02-14 15:25:34 +0000

    Add Fabric, upload builds with circleci

--- app/src/main/kotlin/com/emmaguy/audiocastradio/App.kt
+++ app/src/main/kotlin/com/emmaguy/audiocastradio/App.kt
@@ -1,10 +1,12 @@
 package com.emmaguy.audiocastradio
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.google.android.gms.cast.ApplicationMetadata
 import com.google.android.gms.cast.MediaStatus
 import com.google.android.libraries.cast.companionlibrary.cast.VideoCastManager
 import com.google.android.libraries.cast.companionlibrary.cast.callbacks.VideoCastConsumerImpl
+import io.fabric.sdk.android.Fabric
 import org.json.JSONObject
 import timber.log.Timber
 
@@ -14,6 +16,9 @@ class App() : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this;
+
+        Fabric.with(this, Crashlytics());
+        Crashlytics.setString("Git SHA", BuildConfig.GIT_SHA);
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())

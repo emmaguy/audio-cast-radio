4854ab80a1dd2b811392b15f85b5ba111091c5a9
Emma Guy <emmag87@gmail.com>
2016-11-19 23:39:53 +0000

    Use Firebase Test Lab's aar for screenshots (#10)

--- app/src/androidTest/kotlin/com/emmaguy/audiocastradio/feature/audiostream/AudioStreamListActivityTest.kt
+++ app/src/androidTest/kotlin/com/emmaguy/audiocastradio/feature/audiostream/AudioStreamListActivityTest.kt
@@ -4,7 +4,7 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.emmaguy.audiocastradio.di.Inject
 import com.emmaguy.audiocastradio.di.Injector
-import com.squareup.spoon.Spoon
+import com.google.android.libraries.cloudtesting.screenshots.ScreenShotter
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,7 +14,6 @@ class AudioStreamListActivityTest : Injector by Inject.instance {
     @Rule @JvmField val rule = ActivityTestRule(AudioStreamListActivity::class.java)
 
     @Test fun takeScreenshot() {
-        Spoon.screenshot(rule.activity, "audio_stream_list_idle")
-//        onView(withId(R.id.audioStreamListRecyclerView)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+        ScreenShotter.takeScreenshot("audio_stream_list_idle", rule.activity)
     }
 }
